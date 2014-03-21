9b8a44b948b003fa39030341dc3e0335a51d7d7c
Ilya Radchenko <ilya@burstcreations.com>
2014-03-21 14:26:03 -0400

    fix initializer?

--- src/initializer.js
+++ src/initializer.js
@@ -3,6 +3,7 @@ Ember.Application.initializer({
 
   initialize: function(container, application) {
     container.register('component:pagination-pager', PaginationPagerComponent);
+    container.register('controller:paginationPage', PaginationPageController);
     application.inject('component:pagination-pager', 'controller:paginationPage', PaginationPageController);
   }
 });
