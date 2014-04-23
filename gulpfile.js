b3b0ec0efb7263c62c74340e3e36970c8ff0a241
Ilya Radchenko <ilya@burstcreations.com>
2014-04-23 12:35:19 -0400

    example test/demo html, gulp dev

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var rename = require('gulp-rename');
 var clean = require('gulp-clean');
 var wrap = require('gulp-wrap');
 var handlebars = require('gulp-ember-handlebars');
+var livereload = require('gulp-livereload');
+var serve = require('gulp-serve');
+var name = 'pagination-pager';
 
 gulp.task('clean-dist', function () {
   gulp.src('dist', { read: false })
@@ -16,24 +19,37 @@ gulp.task('templates', function(){
     .pipe(handlebars({
       outputType: 'browser',
       processName: function () {
-        return 'components/pagination-pager';
+        return 'components/' + name;
       }
      }))
-    .pipe(rename('pagination-pager.template.js'))
+    .pipe(rename(name + '.template.js'))
     .pipe(gulp.dest('dist'))
     .pipe(uglify())
-    .pipe(rename('pagination-pager.template.min.js'))
+    .pipe(rename(name + '.template.min.js'))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('scripts', function () {
   gulp.src(['src/item-controller.js', 'src/component.js', 'src/initializer.js'])
-    .pipe(concat('pagination-pager.js'))
+    .pipe(concat(name + '.js'))
     .pipe(wrap('(function (Ember) {\n<%= contents %>\n}(window.Ember));'))
     .pipe(gulp.dest('dist'))
     .pipe(uglify())
-    .pipe(rename('pagination-pager.min.js'))
+    .pipe(rename(name + '.min.js'))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', serve(['test', 'dist']));
+
+gulp.task('dev', function() {
+  var server = livereload();
+
+  gulp.start('default');
+  gulp.start('serve');
+  gulp.watch('src/*', ['templates', 'scripts']);
+  gulp.watch('dist/*.js').on('change', function(file) {
+    server.changed(file.path);
+  });
 });
 
 gulp.task('default', ['clean-dist', 'templates', 'scripts']);
