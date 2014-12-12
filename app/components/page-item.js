c4079b4ba48d2b4f5bc325037a765393c4db8426
Ilya Radchenko <ilya@burstcreations.com>
2014-12-12 16:25:59 -0500

    initial gh-pages commit

---
+++ app/components/page-item.js
@@ -0,0 +1,4 @@
+import Ember from 'ember';
+import PageItem from 'pagination-pager/components/page-item';
+
+export default PageItem;
---
+++ app/components/pagination-pager.js
@@ -0,0 +1,4 @@
+import Ember from 'ember';
+import PaginationPager from 'pagination-pager/components/pagination-pager';
+
+export default PaginationPager;
