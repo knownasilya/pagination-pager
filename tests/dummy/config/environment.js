ec830ed582a886529e2484f66d046d19776a4fb3
Ilya Radchenko <ilya@burstcreations.com>
2014-12-12 16:28:03 -0500

    change dummy baseurl for gh-pages

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
+    baseURL: '/pagination-pager',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
