a28fd69351c21ab7f41ed7af55f36bf60607b128
Vadim Kazakov <vkazakov@sugarcrm.com>
2016-05-05 22:04:51 -0600

    fix for autoHide logic
    currently autoHide isn’t working due to incorrect operator precedence

--- addon/components/pagination-pager.js
+++ addon/components/pagination-pager.js
@@ -93,7 +93,7 @@ export default Ember.Component.extend({
     var count = this.get('count');
     var hide = this.get('hide');
 
-    return hide || (autoHide && !count || count <= 1);
+    return hide || (autoHide && (!count || count <= 1));
   }),
 
   pages: computed('count', 'current', 'countOut', 'countIn', function () {
--- tests/unit/components/pagination-pager-test.js
+++ tests/unit/components/pagination-pager-test.js
@@ -4,8 +4,7 @@ import {
 } from 'ember-qunit';
 
 moduleForComponent('pagination-pager', 'PaginationPagerComponent', {
-  // specify the other units that are required for this test
-  // needs: ['component:foo', 'helper:bar']
+  needs: ['component:page-item']
 });
 
 test('it renders', function() {
@@ -18,4 +17,34 @@ test('it renders', function() {
   // appends the component to the page
   this.append();
   equal(component._state, 'inDOM');
+});
+
+moduleForComponent('pagination-pager', 'PaginationPagerComponent - autoHide', {
+  needs: ['component:page-item']
+});
+
+function autoHideSetup(autoHide, count) {
+  var component = this.subject();
+  component.set('autoHide', autoHide);
+  component.set('count', count);
+  this.append();
+  return component;
+}
+
+test('when set to false and count is 1 it sets isHidden to false', function(assert) {
+  var component = autoHideSetup.call(this, false, 1);
+
+  assert.notOk(component.get('isHidden'));
+});
+
+test('when set to true and count is 1 it sets isHidden to true', function(assert) {
+  var component = autoHideSetup.call(this, true, 1);
+
+  assert.ok(component.get('isHidden'));
+});
+
+test('when set to true and count is 2 it sets isHidden to false', function(assert) {
+  var component = autoHideSetup.call(this, true, 2);
+
+  assert.notOk(component.get('isHidden'));
 });
