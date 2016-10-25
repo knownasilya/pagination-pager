9db848207d4a7eec823308497d5a9cb65e0143db
Ilya Radchenko <ilya@burstcreations.com>
2016-10-25 10:50:52 -0400

    Make autohide example toggle autohide prop

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -4,6 +4,8 @@ export default Ember.Controller.extend({
   current: 2,
   dynamicCount: 10,
   dynamicCurrent: 5,
+  hidden: false,
+  autoHide: true,
 
   actions: {
     pageChanged: function (direction, page, last) {
